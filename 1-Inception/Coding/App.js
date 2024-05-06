96963efa01c749fc1beb3257b3df09799bb9ec85
revadisundaram7 <sundaramrevadi@gmail.com>
2024-05-06 15:08:53 +0530

    Committing first

---
+++ 1-Inception/Coding/App.js
@@ -0,0 +1,21 @@
+//Creating an element(h1) in React 
+// const heading = React.createElement("h1", {
+//     id: "heading",
+//     xyz: "abc"
+// }, "Hello World from React!");
+
+const parent = React.createElement("div",{ id: "parent"}, 
+React.createElement("div", {id: "child"}, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag")
+]
+));
+
+//Output is javascript object
+console.log(parent);
+
+//Put this DOM to the root
+const root = ReactDOM.createRoot(document.getElementById("root") );
+//Creating the root inside in React (used for render) 
+//Takes the js object and put it into root element as a h1 tag
+root.render(parent);
