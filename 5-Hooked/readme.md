fdd89fe0a51b1592ea61045e65620eecbb0e6d6b
revadisundaram7 <sundaramrevadi@gmail.com>
2024-06-14 14:27:56 +0530

    Committing Episode-5 Hooked

---
+++ 5-Hooked/readme.md
@@ -0,0 +1,22 @@
+# Best Practice
+- Make separate files for separate components
+- extension .js == .jsx, its the same, it harly matters
+- import and export if using separate components
+
+/*
+* Header
+- Logo
+- Nav Items
+* Body
+- Search Input
+- * Restraurant Container
+        - Image
+        - Name of Res, Star rating, cuisine, delivery time
+- Res Card
+* Footer
+- Copyright
+- Links
+- Address
+- Contact
+
+*/
---
+++ 5-Hooked/src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () =>{
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
